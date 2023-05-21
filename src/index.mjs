3a3b5148dcbd6647d7cdda77a742fa6232121710
chenyuhao <cccchenyuhao@gmail.com>
2023-05-22 06:32:47 +0800

    first script

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,9 @@
-console.log(1);
+#!/usr/bin/env node
+
+import { spawn } from "node:child_process";
+
+spawn("echo", ["Command starts"], {
+  stdio: "inherit",
+  shell: true,
+});
+console.log("After spawn()");
