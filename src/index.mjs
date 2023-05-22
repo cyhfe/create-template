324c3add85176cb810eeb4677069d70430939b1e
chenyuhao <cccchenyuhao@gmail.com>
2023-05-22 08:46:57 +0800

    read file

--- src/index.mjs
+++ src/index.mjs
@@ -5,11 +5,20 @@ import { fileURLToPath } from "node:url";
 
 const templateDir = path.resolve(
   fileURLToPath(import.meta.url),
-  "../..",
+  "../",
   `template`
-)
+);
 
-const dir = fs.readdirSync("./");
+const dir = fs.readdirSync(templateDir);
 console.log(dir);
 
 console.log(templateDir);
+
+const write = (file, content) => {
+  const targetPath = path.join(root, renameFiles[file] ?? file);
+  if (content) {
+    fs.writeFileSync(targetPath, content);
+  } else {
+    copy(path.join(templateDir, file), targetPath);
+  }
+};
