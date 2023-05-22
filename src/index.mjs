7e3bd9d6f72194a54a075b739fcc68c40f9e61b0
chenyuhao <cccchenyuhao@gmail.com>
2023-05-22 09:35:57 +0800

    first version

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
+import { log } from "node:console";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+
+const repoName = process.argv[2];
+
+const root = path.resolve(process.cwd(), repoName);
 
 const templateDir = path.resolve(
   fileURLToPath(import.meta.url),
@@ -9,16 +14,36 @@ const templateDir = path.resolve(
   `template`
 );
 
-const dir = fs.readdirSync(templateDir);
-console.log(dir);
-
-console.log(templateDir);
-
 const write = (file, content) => {
-  const targetPath = path.join(root, renameFiles[file] ?? file);
+  const targetPath = path.join(root, file);
   if (content) {
     fs.writeFileSync(targetPath, content);
   } else {
     copy(path.join(templateDir, file), targetPath);
   }
 };
+
+const files = fs.readdirSync(templateDir);
+fs.mkdirSync(root, { recursive: true });
+
+for (const file of files) {
+  write(file);
+}
+
+function copy(src, dest) {
+  const stat = fs.statSync(src);
+  if (stat.isDirectory()) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
+
+function copyDir(srcDir, destDir) {
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    copy(srcFile, destFile);
+  }
+}
