34b0584218ad1519268824858acf6077c18f5338
chenyuhao <cccchenyuhao@gmail.com>
2023-05-28 05:56:09 +0800

    feat: prompts

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,76 @@
 #!/usr/bin/env node
 
+import { log } from "node:console";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import prompts from "prompts";
+import {
+  blue,
+  cyan,
+  green,
+  lightGreen,
+  lightRed,
+  magenta,
+  red,
+  reset,
+  yellow,
+} from "kolorist";
 
-const repoName = process.argv[2];
+const TEMPLATES = [
+  {
+    name: "react",
+    display: "webpack-react-typescript",
+    color: cyan,
+  },
+  {
+    name: "express",
+    display: "express-typescript",
+    color: yellow,
+  },
+];
+let response;
+try {
+  response = await prompts([
+    {
+      type: "text",
+      name: "projectName",
+      message: reset("Project name:"),
+      initial: "template",
+    },
+    {
+      type: "select",
+      name: "template",
+      message: reset("template"),
+      initial: 0,
+      choices: TEMPLATES.map((template) => {
+        const color = template.color;
+        return {
+          title: color(template.display),
+          value: template.name,
+        };
+      }),
+    },
+    {
+      onCancel: () => {
+        console.log(red("✖") + " Operation cancelled");
+      },
+    },
+  ]);
+} catch (error) {
+  console.log(error.message);
+}
 
-const root = path.resolve(process.cwd(), repoName);
+const { projectName, template } = response;
+
+if (!projectName || !template) process.exit(-1);
+
+const root = path.resolve(process.cwd(), projectName);
 
 const templateDir = path.resolve(
   fileURLToPath(import.meta.url),
   "../",
-  `template`
+  `template-${template}`
 );
 
 const write = (file, content) => {
@@ -30,11 +89,16 @@ for (const file of files) {
   write(file);
 }
 
+console.log("done");
+console.log("happy coding!");
+
 function copy(src, dest) {
   const stat = fs.statSync(src);
   if (stat.isDirectory()) {
+    console.log("copy dir to " + dest);
     copyDir(src, dest);
   } else {
+    console.log("copy file to " + dest);
     fs.copyFileSync(src, dest);
   }
 }
--- src/template-express/webpack.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-    hot: true,
-  },
-};
